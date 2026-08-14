5b6af1539c42ac7463d1c3b5b9875835dfabf18c
AbdiElmi <abdi.elmi@rocketmail.com>
2026-08-14 14:33:06 -0400

    updated App.vue to fetch data successfully

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,33 @@
-<script setup>
-</script>
-
 <template>
   <div class="container">
-    <h1>PRESTO Travel Map</h1>
-    <p>Visualizing my 2026 transit history across Toronto and the GTA.</p>
+    <!-- replace with movie logo but with presto -->
+    <h1>dude, where's my presto</h1>
+
+    <!-- replace with loading image -->
+    <p v-if="loading">Loading trips...</p>
+
+    <div v-else>
+      <p><strong>Total Trips:</strong> {{ trips.length }}</p>
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const trips = ref([]);
+const loading = ref(true);
+
+onMounted(async () => {
+  try {
+    const response = await fetch('http://localhost:3001/api/trips');
+    const data = await response.json();
+
+    trips.value = data;
+  } catch (error) {
+    console.error('failed to load trips:', error);
+  } finally {
+    loading.value = false;
+  }
+});
+</script>
