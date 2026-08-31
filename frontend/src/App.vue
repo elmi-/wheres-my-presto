156d4f9ce76405dfa23499b2ff71f285565e0a97
AbdiElmi <abdi.elmi@rocketmail.com>
2026-08-31 15:51:14 -0400

    started styling app and added logo and deleted generic css added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -22,10 +22,68 @@ onMounted(async () => {
 <template>
   <div class="container">
     <!-- replace with movie logo but with presto -->
-    <h1>dude, where's my presto</h1>
-
-    <!-- replace with loading image -->
-    <p v-if="loading">Loading trips...</p>
-    <TravelMap />
+    <!-- <h1>dude, where's my presto</h1> -->
+     <header class="dashboard-header">
+      <!-- move filters from travelmap -->
+      <!-- <div class="dashboard-filters"></div> -->
+       <div class="logo-container">
+        <img src="./assets/presto-app-logo.png" alt="dude, where's my presto" />
+       </div>
+     </header>
+     <section class="dashboard-content">
+      <!-- replace with loading image -->
+      <p v-if="loading">Loading trips...</p>
+      <TravelMap />
+     </section>
   </div>
 </template>
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html,
+body,
+#app {
+  margin: 0;
+  padding: 0 1%;
+  min-height: 100%;
+  background: white;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.app {
+  min-height: 100vh;
+  background: white;
+}
+
+.dashboard-header {
+  width: 100%;
+  display: flex;
+}
+
+.logo-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.logo-container img {
+  max-height: 150px;
+  width: auto;
+  object-fit: contain;
+}
+
+@media (max-width: 600px) {
+  .dashboard-header {
+    justify-content: center;
+  }
+  .logo-container img  {
+    max-width: 250px;
+    text-align: center;
+  }
+}
+</style>
